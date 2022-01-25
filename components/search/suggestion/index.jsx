15069b45d0c5e9c00d962d085aad1f44ce244358
Rahul5430 <rahul213himani@gmail.com>
2022-01-25 23:22:02 +0530

    added NoItemFound component

--- components/search/suggestion/index.jsx
+++ components/search/suggestion/index.jsx
@@ -1,69 +1,83 @@
-import React, { useState } from 'react';
-import { FlatList, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+	Image,
+	SafeAreaView,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
 
 // definition of the Item, which will be rendered in the FlatList
 const Item = ({ item }) => {
-    const { name, symbol, image, price, changeRate } = item;
+	const { name, symbol, image, price, changeRate } = item;
 
-    return (
-        <View style={styles.item}>
-            <Text style={styles.title}>{name}</Text>
-            <Text style={styles.details}>{symbol}</Text>
-            <Text style={styles.details}>{image}</Text>
-            <Text style={styles.details}>{price}</Text>
-            <Text style={styles.details}>{changeRate}</Text>
-        </View>
-    );
-}
+	return (
+		<View style={styles.item}>
+			<Text style={styles.title}>{name}</Text>
+			<Text style={styles.details}>{symbol}</Text>
+			<Text style={styles.details}>{image}</Text>
+			<Text style={styles.details}>{price}</Text>
+			<Text style={styles.details}>{changeRate}</Text>
+		</View>
+	);
+};
 
 const NoItemFound = ({ searchPhrase }) => {
-    return (
-        <View style={styles.imageContainer}>
-            <Image 
-                style={styles.image}
-                source={require('../../../assets/search-result-not-found-2130361-1800925.png')}
-            />
-            <Text style={styles.notFoundText}>Sorry, No results found for "{searchPhrase}"</Text>
-        </View>
-    );
+	return (
+		<View style={styles.imageContainer}>
+			<Image
+				style={styles.image}
+				source={require('../../../assets/search-result-not-found-2130361-1800925.png')}
+			/>
+			<Text style={styles.notFoundText}>
+				Sorry, No results found for "{searchPhrase}"
+			</Text>
+		</View>
+	);
 };
 
 const List = (props) => {
-    // const [found, setFound] = useState(false);
-
-    const renderItem = ({ item }) => {
-		// when no input, show all
-		if (props.searchPhrase === '') {
-            // setFound(true);
-            return <Item item={item} />;
-		}
-        // filter of the name
-        if (item.name.toUpperCase().includes(props.searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
-            // setFound(true);
-            return <Item item={item} />;
-        }
-        // filter of the description
-        if (item.symbol.toUpperCase().includes(props.searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
-            // setFound(true);
-            return <Item item={item} />;
-        }
-        // setFound(false);
+	const filterData = (data) => {
+		return data.filter(
+			(item) =>
+				props.searchPhrase === '' ||
+				item.name
+					.toUpperCase()
+					.includes(
+						props.searchPhrase
+							.toUpperCase()
+							.trim()
+							.replace(/\s/g, '')
+					) ||
+				item.symbol
+					.toUpperCase()
+					.includes(
+						props.searchPhrase
+							.toUpperCase()
+							.trim()
+							.replace(/\s/g, '')
+					)
+		);
 	};
 
 	return (
 		<SafeAreaView style={styles.list__container}>
-			<View
-				onStartShouldSetResponder={() => {
-					props.setClicked(false);
-				}}
-			>
-				<FlatList
-					data={props.data}
-					renderItem={renderItem}
-					keyExtractor={(item) => item.id}
-				/>
-                {/* {!found && <NoItemFound searchPhrase={props.searchPhrase} />} */}
-			</View>
+			<ScrollView>
+				<View
+					onStartShouldSetResponder={() => {
+						props.setClicked(false);
+					}}
+				>
+					{!filterData(props.data).length ? (
+						<NoItemFound searchPhrase={props.searchPhrase} />
+					) : (
+						filterData(props.data).map((item) => (
+							<Item key={item.id} item={item} />
+						))
+					)}
+				</View>
+			</ScrollView>
 		</SafeAreaView>
 	);
 };
@@ -87,18 +101,17 @@ const styles = StyleSheet.create({
 		marginBottom: 5,
 		fontStyle: 'italic',
 	},
-    image: {
-        alignSelf: 'center',
-        width: '100%',
-        height: '100%',
-        resizeMode: 'center',
-    },
-    imageContainer: {
-        width: '95%',
-        height: 300,
-        alignSelf: 'center',
-    },
-    notFoundText: {
-        textAlign: 'center',
-    },
+	image: {
+		alignSelf: 'center',
+		resizeMode: 'center',
+	},
+	imageContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+		flexDirection: 'column',
+	},
+	notFoundText: {
+		textAlign: 'center',
+	},
 });
