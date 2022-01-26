9fce611558f15db11a2e3cad00d689140a26fef2
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-01-26 23:03:15 +0530

    Cryptocurrency api added

--- screens/StockScreen.js
+++ screens/StockScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { 
     Button, 
     FlatList, 
@@ -15,21 +15,34 @@ import Header from "../components/header/headerLoginPage";
 export default function StockScreen() {
     const currencies = data.currencies;
     const { logout } = useAuth();
-
+    const [cryptoData,setCrytpoData] = useState({ data : []});
+    const a=10;
+    
+    useEffect(()=> {
+        fetch("https://data.messari.io/api/v2/assets?fields=id,symbol,name,metrics/market_data",
+        {
+            method:"GET",
+            headers: { "x-messari-api-key": "9f3cc221-44ec-460a-bf7a-8576d8e6dcf9" }
+        }).then(response => response.json())
+        .then(jsonResponse => setCrytpoData(jsonResponse))
+        .catch(error => console.log(error))
+        .finally(() => console.log(cryptoData));
+    },[a]);
+    
     return (
         <SafeAreaView style={styles.screen}>
             <Header />
             <FlatList 
-                data={currencies}
+                data={cryptoData.data}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
                     <Listcard 
                         title={item.symbol}
                         subTitle={item.name}
-                        image={item.image}
+                        image={`https://messari.io/asset-images/${item.id}/128.png`}
                         onPress={() => console.log(item)}
-                        price={item.price}
-                        changeRate={item.changeRate}
+                        price={(item.metrics.market_data.price_usd<1.01)?Math.round(item.metrics.market_data.price_usd * 1000000)/1000000:Math.round(item.metrics.market_data.price_usd * 100)/100 }
+                        changeRate={Math.round(item.metrics.market_data.percent_change_usd_last_24_hours* 100)/100 }
                     />
                 )}
             />
